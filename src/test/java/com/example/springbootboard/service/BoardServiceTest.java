7de2df9cc48f522e1385c529505298d8a4ba9baf
Kimminhyeo <ualsgur99@gmail.com>
2023-02-21 22:43:36 +0900

    검색-01

---
+++ src/test/java/com/example/springbootboard/service/BoardServiceTest.java
@@ -0,0 +1,64 @@
+package com.example.springbootboard.service;
+
+import com.example.springbootboard.dto.BoardDTO;
+import com.example.springbootboard.dto.PageRequestDTO;
+import com.example.springbootboard.dto.PageResultDTO;
+import com.example.springbootboard.entity.Board;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoardServiceTest {
+    @Autowired
+    private BoardService boardService;
+
+    @Test
+    public void testGet(){
+        Long bno = 100L;
+
+        BoardDTO boardDTO = boardService.get(bno);
+
+        System.out.println(boardDTO);
+    }
+
+    @Test
+    public void testRegister(){
+        BoardDTO dto = BoardDTO.builder()
+                .title("Test.")
+                .content("Test...")
+                .writerEmail("user55@aaa.com")
+                .build();
+
+        Long bno = boardService.register(dto);
+    }
+
+    @Test
+    public void testList(){
+        PageRequestDTO pageRequestDTO = new PageRequestDTO();
+
+        PageResultDTO<BoardDTO, Object[]> result = boardService.getList(pageRequestDTO);
+
+        for(BoardDTO boardDTO : result.getDtoList()){
+            System.out.println(boardDTO);
+        }
+    }
+
+    @Test
+    public void testRemove(){
+        Long bno = 1L;
+
+        boardService.removeWithReplies(bno);
+    }
+
+    @Test
+    public void testModify(){
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(2L)
+                .title("제목 변경합니다.")
+                .content("내용 변경합니다.")
+                .build();
+
+        boardService.modify(boardDTO);
+    }
+}
