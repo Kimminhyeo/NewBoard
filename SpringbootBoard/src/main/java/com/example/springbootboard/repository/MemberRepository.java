422af2f2762ba396f7bab14fb05651afc8b4256d
김민혁 <94088147+Kimminhyeo@users.noreply.github.com>
2023-02-21 22:42:44 +0900

    Delete SpringbootBoard directory

--- SpringbootBoard/src/main/java/com/example/springbootboard/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootboard.repository;
-
-import com.example.springbootboard.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-}
